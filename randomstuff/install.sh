ba5360fb10fde9828b42c20ac4ef6735df1bc63f
nicco1690 <78063037+nicco1690@users.noreply.github.com>
2023-05-28 23:36:58 +0000

    install part 2 (this stuff hasnothing to do with the website)

--- randomstuff/install.sh
+++ randomstuff/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-yay -S picom-git awesome-git acpid git mpd ncmpcpp wmctrl \
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+yay -S cronie picom-git awesome-git acpid git mpd ncmpcpp wmctrl \
 firefox lxappearance gucharmap thunar alacritty neovim polkit-gnome \
 xdotool xclip scrot brightnessctl alsa-utils pulseaudio jq acpi rofi \
-inotify-tools zsh mpdris2 bluez bluez-utils bluez-plugins acpi acpi_call \
-playerctl redshift cutefish-cursor-themes-git cutefish-icons upower xorg xorg-init tar
+inotify-tools zsh mpDris2 bluez bluez-utils bluez-plugins acpi acpi_call \
+playerctl redshift cutefish-cursor-themes-git cutefish-icons upower xorg tar \
+feh imagemagick maim gpick gcalculator neofetch discord steam heroic-games-launcher-bin \
+betterdiscordctl python-mutagen libnotify python ruby-git lua ly rofi lxappearance\
 
 cd 
 mkdir .backup_config
@@ -13,7 +20,7 @@ cp -r ~/.ncmpcpp .backup_config/
 cp -r ~/.themes .backup_config/
 
 cd
-git clone --recurse-submodules https://github.com/saimoomedits/dotfiles.git
+git clone --recurse-submodules https://github.com/d-solis/dotfiles/tree/awesome
 cd dotfiles
 
 cp -rf .config/* ~/.config/
@@ -34,5 +41,10 @@ cd ~/.config/awesome/misc
 sudo chmod -R +x *
 
 systemctl --user enable mpd
+systemctl --user enable mpDris2
+sudo systemctl enable cronie
 sudo systemctl enable bluetooth
-reboot -t 0
+sudo systemctl enable wpa_supplicant
+
+sudo reboot --reboot -f
+#This bash script reboots your system at the end of its installation, make sure to always close and save all applications beforehand, i am not responsible for any lost data.
